78f90c4cf256d65afe7fc099e70dfbe84e76f8f6
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-07-21 06:19:14 +0300

    Add files via upload

---
+++ check_the_validity_of_a_password/check_the_validity_of_a_password.py
@@ -0,0 +1,32 @@
+import re
+password = input()
+flag = 0
+while True:
+    if len(password) < 8:
+        flag = -1
+        print("Not a Valid Password")
+        break
+    elif not re.search("[a-z]", password):
+        flag = -1
+        print("Not a Valid Password")
+        break
+    elif not re.search("[A-Z]", password):
+        flag = -1
+        print("Not a Valid Password")
+        break
+    elif not re.search("[0-9]", password):
+        flag = -1
+        print("Not a Valid Password")
+        break
+    elif not re.search("[_@$]", password):
+        flag = -1
+        print("Not a Valid Password")
+        break
+    elif re.search("\s", password):
+        flag = -1
+        print("Not a Valid Password")
+        break
+    else:
+        flag = 0
+        print("Valid Password")
+        break
