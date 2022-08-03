53a1822fc2bf3a408b8d592283a5290ff7aa04f2
Miroslav <97790728+MiroSultanov@users.noreply.github.com>
2022-08-03 06:29:14 +0300

    Update mad_libs_generator_game.py

--- mad_libs_generator_game/mad_libs_generator_game.py
+++ mad_libs_generator_game/mad_libs_generator_game.py
@@ -1,6 +1,10 @@
+# Mad libs generator is a fun game that is usually played by kids.
+
+# In this python game user has to enter substitutes for blanks in the story without knowing the story. It will be fun to read aloud the stories after 
+# filling the blanks.
+
 #import module
-from tkinter
-import *
+from tkinter import *
 
 # initialize window
 root = Tk()
